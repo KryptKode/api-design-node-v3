fb8d87ed4c69246268b5b0eb50b6c472653fa8fa
kryptkode <paulakhamiogu@gmail.com>
2020-03-11 22:29:24 +0100

    Created basic server with routes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
-import express from 'express'
+import express, { Router } from 'express'
 import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
 import cors from 'cors'
+
+import config from './config'
 
 export const app = express()
 
@@ -12,4 +14,26 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+const router = Router()
+router.get('/me', (req, res) => {
+  res.send('This is the /me endpoing')
+})
+
+const log = (req, res, next) => {
+  console.log('Logger')
+  next()
+}
+
+app.use('/api', router)
+
+app.get('/data', [log, log, log], (req, res) => {
+  res.send({ data: 'ME' })
+})
+
+const PORT = config.port
+
+export const start = () => {
+  app.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}`)
+  })
+}

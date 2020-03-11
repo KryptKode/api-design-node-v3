99ad9b434bd7bc0b53243e8655f551e86a0407ff
kryptkode <paulakhamiogu@gmail.com>
2020-03-11 23:07:21 +0100

    Add item routes and connect to server

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,26 @@
-export default {}
+const getItems = (req, res) => {
+  res.send('Get all items')
+}
+
+const getItem = (req, res) => {
+  res.send(`Get item with id ${req.params.id}`)
+}
+
+const createItem = (req, res) => {
+  res.send('Created item successfully')
+}
+
+const updateItem = (req, res) => {
+  res.send('Updated item successfully')
+}
+
+const deleteItem = (req, res) => {
+  res.send('Deleted item successfully')
+}
+export default {
+  getItems,
+  getItem,
+  createItem,
+  updateItem,
+  deleteItem
+}
--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,17 @@
 import { Router } from 'express'
+import controllers from './item.controllers'
 
 const router = Router()
+
+router
+  .route('/')
+  .get(controllers.getItems)
+  .post(controllers.createItem)
+
+router
+  .route('/:id')
+  .get(controllers.getItem)
+  .delete(controllers.deleteItem)
+  .put(controllers.updateItem)
 
 export default router

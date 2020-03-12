1e4c8d1623b37ab5ded7c205e63cef975e61d531
kryptkode <paulakhamiogu@gmail.com>
2020-03-12 10:53:37 +0100

    Created generic CRUD methods

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,16 @@
-export default {}
+import Item from './item.model'
+import { crudControllers } from '../../utils/crud'
+
+const getOne = () => crudControllers(Item).getOne()
+const getMany = () => crudControllers(Item).getMany()
+const createOne = () => crudControllers(Item).createOne()
+const removeOne = () => crudControllers(Item).removeOne()
+const updateOne = () => crudControllers(Item).updateOne()
+
+export default {
+  getOne,
+  getMany,
+  createOne,
+  removeOne,
+  updateOne
+}
--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,12 +1,64 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
 
-export const getMany = model => async (req, res) => {}
+  const item = await model.findOne({ _id: id, createdBy: userId }).exec()
 
-export const createOne = model => async (req, res) => {}
+  if (!item) {
+    return res.status(400).end()
+  }
 
-export const updateOne = model => async (req, res) => {}
+  return res.status(200).json({ data: item })
+}
 
-export const removeOne = model => async (req, res) => {}
+export const getMany = model => async (req, res) => {
+  const userId = req.user._id
+
+  const items = await model.find({ createdBy: userId }).exec()
+  return res.status(200).json({ data: items })
+}
+
+export const createOne = model => async (req, res) => {
+  const userId = req.user._id
+  const body = req.body
+  const modelInfo = { ...body, createdBy: userId }
+
+  const result = await model.create(modelInfo)
+  return res.status(201).json({ data: result })
+}
+
+export const updateOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
+  const body = req.body
+
+  const result = await model
+    .findOneAndUpdate({ _id: id, createdBy: userId }, body, {
+      new: true
+    })
+    .exec()
+
+  if (!result) {
+    return res.status(400).end()
+  }
+
+  return res.status(200).json({ data: result })
+}
+
+export const removeOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
+
+  const result = await model
+    .findOneAndRemove({ _id: id, createdBy: userId })
+    .exec()
+
+  if (!result) {
+    return res.status(400).end()
+  }
+
+  return res.status(200).json({ data: result })
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
